a437e3ed9552d3ccf2c1cb97f146ae84ae02e5ba
imsachinkumar <122716893+imsachinkumar@users.noreply.github.com>
2025-05-26 10:14:07 +0530

    Create dec_to_bin.java

---
+++ dec_to_bin.java
@@ -0,0 +1,15 @@
+public class dec_to_bin {
+    public static void main(String[] args) {
+        int n=45;
+        int sum=0;
+        int mul=1;
+
+        while(n>0){
+            int rem=n%2;
+            sum=sum+rem*mul;
+            n=n/2;
+            mul=mul*10;
+        }
+        System.out.println(sum);
+    }
+}
